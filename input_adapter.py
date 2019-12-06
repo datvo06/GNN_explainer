bea299b4e33111d9a150b444462d13f3476ea139
Dini <chingting.wu@cinnamon.is>
2019-12-06 08:10:24 +0000

    Drafting the outline

---
+++ input_adapter.py
@@ -0,0 +1,23 @@
+from __future__ import print_function, unicode_literals
+import torch
+
+
+def convert_doc_graph_into_explainer_graph(V, A):
+	pass
+
+def save_model_for_gnn_explainer(model):
+	"""
+	Take input as a trained torch model, save it to the format of gnn explainer
+	"""
+	pass
+
+class DataLoaderForExplainer:
+	"""
+	This class allows us to iterate, either through each document or return the whole
+	document graph set similar to the original code
+	"""
+
+class PyTorchGCNModel(torch.nn.Model):
+	pass
+
+
