da7fe330d6fd0391498a6850931d44274c5d4ea9
dini <chingting.wu@cinnamon.is>
2019-12-10 13:23:33 +0800

    Modified draw functions.

--- test_file.py
+++ test_file.py
@@ -29,10 +29,7 @@ if __name__ == '__main__':
     input_data = read_pickle(
         os.path.join(features_folder, pickle_path)
     )
-    # bow = load_json(
-    #     os.path.join(features_folder, bow_path)
-    # )
-
+    corpus = open(os.path.join(features_folder, bow_path)).read()[1:-2]
     class_kv = preprocess_class(os.path.join(features_folder, class_path))
 
     i = 0
@@ -46,11 +43,12 @@ if __name__ == '__main__':
                             list_positions=(coord*1000).astype(int),
                             adj_mats=adj,
                             node_labels=label_y,
-                            node_importances= N*[1],
+                            node_importances=N*[1],
                             # position_importances=N*[4*[1]],
                             position_importances=N*[1],
                             bow_importances=N*[bow.shape[-1] * [1]],
-                            adj_importances=1
+                            adj_importances=1,
+                            word_list=corpus
                             )
 
     cv2.imshow('My Image', image)
--- utils/draw_utils.py
+++ utils/draw_utils.py
@@ -10,9 +10,10 @@ import cv2
 
 currdir = os.path.dirname(os.path.abspath(__file__))
 
+fonts = []
 for i in range(1, 12, 2):
-    fonts = ImageFont.truetype(
-        os.path.join(currdir, "arial-unicode-ms.ttf"), i)
+    fonts.append(ImageFont.truetype(
+        os.path.join(currdir, "arial-unicode-ms.ttf"), i))
 
 
 def get_used_font(top_height):
@@ -41,6 +42,7 @@ def draw_bbox(img, x, y, w, h, color, thickness=5):
     y = int(y)
     w = int(w)
     h = int(h)
+    print(x, y, w, h)
     return cv2.rectangle(img, (x, y), (x+w, y+h), color, thickness)
 
 
@@ -76,16 +78,16 @@ def draw_text(img, x, y, w, h, text, color=(0, 0, 0),
     font = get_used_font(h)
     pil_img = Image.fromarray(img)
     draw = ImageDraw.Draw(pil_img)
-    charwidth = font.font.width
+    charwidth = font.size
     if importances is None:
         draw.text(
-            (x, y+h), text, font=get_used_font(),
+            (x, y+h), text, font=font,
             fill=(color))
     else:
         for i, character in enumerate(text):
             draw.text(
                 (x+i*charwidth+1, y+h),
-                character, font=get_used_font(),
+                character, font=font,
                 fill=(
                     tuple(
                         [int(127-0.5*color_elem*(importances[i]))
@@ -102,16 +104,18 @@ def draw_node_text_importances(img, list_bboxs, list_texts,
     get_mapping = (lambda c: c)\
         if bow_dict is None else (lambda c: bow_dict[c])
     for i, bbox in enumerate(list_bboxs):
+        print(list_texts[i])
         img = draw_text(
             img, bbox[0], bbox[1], bbox[2], bbox[3],
             list_texts[i],
             importances=[bow_importances[i][get_mapping(c)]
-                         for c in list_texts])
+                               for c in list_texts[i]])
     return img
 
 
 def get_pseudo_text_bow_repr(bows, word_list):
-    return str([word_list[i] for i in bows if i != 0])
+    concated_string = ''.join([word_list[i] for i, val in enumerate(bows) if val != 0])
+    return concated_string
 
 
 def draw_arrow(img, x1, y1, x2, y2, color, thickness=1):
@@ -204,10 +208,10 @@ def visualize_graph(list_bows, list_positions,
                     word_list=None, is_text=False):
     """
     Args:
-        list_bows: the bag of words sets
+        list_bows: the bag of words features
         list_positions: list of (x, y, w, h)
         adj_mats: matrix of NxNxE (E is number of edge types)
-        word_list: the list of word (if we were to visualize bows)
+        word_list: the list of word (if we were to visualize bows), aka corpus
         is_text: if the bows is texts
 
         Node importances: N
@@ -216,13 +220,17 @@ def visualize_graph(list_bows, list_positions,
 
     """
     # First, get the texts for all bows
-    list_texts = [(i for i in range(len(bow)) if bow[i] != 0)
+    list_bows = list_bows.astype(int)
+    list_texts = [str(i for i in range(len(bow)) if bow[i] != 0)
                   for bow in list_bows]
     bow_dict = dict([(word, i) for i, word in enumerate(word_list)]
                     ) if word_list is not None else None
+
+
     if not is_text and word_list is not None:
         list_texts = [get_pseudo_text_bow_repr(bow, word_list)
                       for bow in list_bows]
+    print(list_texts)
     max_x = np.max(list_positions[:, 0] + list_positions[:, 2])
     max_y = np.max(list_positions[:, 1] + list_positions[:, 3])
     if orig_img is None:
