c0035f5b0e7ba3675b8c085f3d87987d3f5ccd21
theengineer <datvo06@gmail.com>
2020-11-17 15:39:37 +0700

    Adding Dataset

--- dump_data_from_torch.py
+++ dump_data_from_torch.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals
 from kv.graphkv_torch.utils.kv_ca_dataset import KV_CA_Dataset
-from kv.graphkv_torch.utils.data_encoder import InputEncoder
+from kv.graphkv_torch.utils.data_encoder import (
+    InputEncoder, convert_label_via_to_cassia, convert_label_to_cassia)
 
 import os
 import sys
@@ -8,6 +9,94 @@ import json
 import numpy as np
 import pickle as pickle
 import glob
+import torch
+
+
+class KV_CA_Dataset_modified(KV_CA_Dataset):
+    def __init__(self,
+                 label_paths: list,
+                 encoder,
+                 classes: list,
+                 clusters: list,
+                 key_types: list,
+                 take_original_input=False
+                 ):
+        """ Dataset class to load CA Format file
+        Params:
+            label_paths (list of path): List of path to labels folder CA Labels
+            encoder: Function that take cassia kv_input
+                and convert to vertex
+            classes: List of classes (Remeber classes[0] for field that we
+            dont care)
+            key_types: List of key type you want to train
+        Optional:
+            graph_builder: Function that take cassia kv_input
+                and convert to adjacency matrix
+            num_graph_channel: The channel of Number of Graph,
+            only support last now
+        """
+        super().__init__(
+            label_paths,
+            encoder,
+            classes,
+            clusters=None,
+            key_types=['key', 'value']
+        )
+
+        self.take_original_input = take_original_input
+
+    def convert_label_to_numerical(self, labels):
+        # Convert labels to index
+        if not self.use_clusters:
+            target = []
+            for class_name, key_type in labels:
+                class_index = self.cls2ind.get(class_name, {}).get(key_type, 0)
+                target.append(class_index)
+            return torch.tensor(target)
+        else:
+            target = []
+            target_cluster = []
+            for cluster_name, class_name, key_type in labels:
+                class_index = self.cls2ind.get(class_name, {}).get(key_type, 0)
+                cluster_index = self.cluster2ind.get(
+                    cluster_name, {}).get(key_type, 0)
+                target.append(class_index)
+                target_cluster.append(cluster_index)
+            return torch.tensor(target), torch.tensor(target_cluster)
+
+    def __getitem__(self, index):
+        label_path = self.label_paths[index]
+        label_data = json.load(open(label_path, encoding='utf-8'))
+
+        if not self.use_cinnamon_format:
+            labels, kv_input = convert_label_via_to_cassia(
+                label_data,
+                use_clusters=self.use_clusters
+            )
+
+            # Convert labels to numerical targets
+            if not self.use_clusters:
+                target = self.convert_label_to_numerical(labels)
+            else:
+                target, target_cluster = self.convert_label_to_numerical(labels)
+        else:
+            labels, kv_input = convert_label_to_cassia(label_data)
+            target = self.convert_label_to_numerical(labels)
+
+        # Convert kv_input to numerical input
+        vertex, adj_matrix = self.encode_kv_input(kv_input)
+
+        if not self.use_clusters:
+            if self.take_original_input:
+                return vertex, adj_matrix, target, kv_input
+            return vertex, adj_matrix, target
+        else:
+            if self.take_original_input:
+                return vertex, adj_matrix, target, target_cluster, kv_input
+            return vertex, adj_matrix, target, target_cluster
+
+    def __len__(self):
+        return self.num_data
 
 
 class InputEncoderKeepKVData(InputEncoder):
@@ -39,7 +128,7 @@ if __name__ == '__main__':
     # third path: class.json path
     all_files = list(glob.glob(os.path.join(sys.argv[1], '*.json')))
     try:
-        dataset = KV_CA_Dataset(all_files,
+        dataset = KV_CA_Dataset_modified(all_files,
                 InputEncoderKeepKVData(json.load(open(sys.argv[2], 'r'))),
                 json.load(open(sys.argv[3])),
                 clusters=None,
@@ -47,7 +136,7 @@ if __name__ == '__main__':
                 take_original_input=True
                 )
     except:
-        dataset = KV_CA_Dataset(all_files,
+        dataset = KV_CA_Dataset_modified(all_files,
                 InputEncoderKeepKVData(json.load(open(sys.argv[2], 'r'))),
                 json.load(open(sys.argv[3])),
                 key_types=['key', 'value'],
